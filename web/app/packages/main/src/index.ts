20ad6618bd7ed5e0b71e37261c7c0a1ad2407745
kasefuchs <kasefuchs@protonmail.com>
2025-03-08 16:04:44 +0300

    chore: add basic styling

--- web/app/packages/main/src/index.ts
+++ web/app/packages/main/src/index.ts
@@ -1,6 +1,8 @@
 import { createElement, render } from "preact";
 import Application from "./pages/Application.tsx";
 
+import "@kasefuchs/website-styles";
+
 const container = document.getElementById("root");
 
 if (container) {
--- web/app/packages/main/src/pages/Application.tsx
+++ web/app/packages/main/src/pages/Application.tsx
@@ -1,7 +1,14 @@
-import { Component, type ReactNode } from "preact/compat";
+import { Component, Fragment, type ReactNode } from "preact/compat";
+import styles from "./Application.module.scss";
 
 export default class Application extends Component {
   public render(): ReactNode {
-    return <div></div>;
+    return (
+      <Fragment>
+        <div className={styles["container"]}>
+          <span>Placeholder</span>
+        </div>
+      </Fragment>
+    );
   }
 }
