76cb3bd0de15f02dccfe5ce5f91704f9580b005c
kasefuchs <kasefuchs@protonmail.com>
2025-06-16 23:05:07 +0300

    feat: add divider component

---
+++ web/packages/components/src/Divider/types.ts
@@ -0,0 +1,11 @@
+import type { JSX } from 'preact'
+
+export type Variant = 'full' | 'middle'
+
+export type Orientation = 'horizontal' | 'vertical'
+
+export type Props = JSX.HTMLAttributes & {
+  element?: JSX.ElementType
+  variant?: Variant
+  orientation?: Orientation
+}
